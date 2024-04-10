06ac1ace5cd6446f12bbc8872499cd81de7cf481
pratishu <pratishu.kr@gmail.com>
2024-04-10 09:14:59 +0530

    init project with vite and TS

---
+++ README.md
@@ -0,0 +1,6 @@
+# About this project
+
+- a simple search component that can be used in Any other places
+- Dropdown menu with search results
+- new page with **title** and **body**
+- on click on search results, it will navigate to new page using ReactRouter and Usenavigate and url will be the `title` that fetched from the result
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React Search component</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
+        voluptatem enim quisquam eum aperiam, velit harum quos cum rem delectus
+        praesentium sunt nemo odit. Necessitatibus debitis maxime quasi ut
+        cumque earum, quos perspiciatis sunt iusto fugit sed? Animi, nesciunt
+        quasi?
+      </p>
+    </>
+  );
+}
+
+export default App;
