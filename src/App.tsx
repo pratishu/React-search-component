cf8b922598a982c9c4b7af6b1497108a294bafbd
pratishu <pratishu.kr@gmail.com>
2024-04-10 09:41:33 +0530

    font and few styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1 className="text-4xl font-medium text-center">
-        React Search component
-      </h1>
+      <h1 className="">React Search component</h1>
       <p className="font-medium text-sm text-gray-500">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
         voluptatem enim quisquam eum aperiam, velit harum quos cum rem delectus
