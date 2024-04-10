15f4773ab41753791677f4863d765c8784051e34
pratishu <pratishu.kr@gmail.com>
2024-04-10 09:24:11 +0530

    tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>React Search component</h1>
-      <p>
+      <h1 className="text-4xl font-medium text-center">
+        React Search component
+      </h1>
+      <p className="font-medium text-sm text-gray-500">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
         voluptatem enim quisquam eum aperiam, velit harum quos cum rem delectus
         praesentium sunt nemo odit. Necessitatibus debitis maxime quasi ut
